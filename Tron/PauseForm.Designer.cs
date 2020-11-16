0d828ffdfed3cf5210a48fe5f7a72a330cbea640
Sammy McNab <samumcna930@ed.amdsb.ca>
2020-11-16 09:45:36 -0500

    PauseForm

--- Tron/PauseForm.Designer.cs
+++ Tron/PauseForm.Designer.cs
@@ -38,9 +38,10 @@
             this.continueButton.BackgroundImage = global::Tron.Properties.Resources.Continue;
             this.continueButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.continueButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.continueButton.Location = new System.Drawing.Point(324, 312);
+            this.continueButton.Location = new System.Drawing.Point(432, 384);
+            this.continueButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.continueButton.Name = "continueButton";
-            this.continueButton.Size = new System.Drawing.Size(316, 62);
+            this.continueButton.Size = new System.Drawing.Size(421, 76);
             this.continueButton.TabIndex = 1;
             this.continueButton.UseVisualStyleBackColor = false;
             this.continueButton.Click += new System.EventHandler(this.continueButton_Click);
@@ -52,9 +53,10 @@
             this.exitButton.BackgroundImage = global::Tron.Properties.Resources.ExitHighlight;
             this.exitButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.exitButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.exitButton.Location = new System.Drawing.Point(12, 305);
+            this.exitButton.Location = new System.Drawing.Point(40, 384);
+            this.exitButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.exitButton.Name = "exitButton";
-            this.exitButton.Size = new System.Drawing.Size(253, 76);
+            this.exitButton.Size = new System.Drawing.Size(310, 76);
             this.exitButton.TabIndex = 0;
             this.exitButton.UseVisualStyleBackColor = false;
             this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
@@ -62,12 +64,14 @@
             // 
             // PauseForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(652, 393);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            this.ClientSize = new System.Drawing.Size(869, 484);
             this.Controls.Add(this.exitButton);
             this.Controls.Add(this.continueButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "PauseForm";
             this.Text = "PauseForm";
             this.ResumeLayout(false);
